8634d841ab6e045b273d0d34eb0cf1e2b5d82fb2
Thullyoo <thullyofilho@gmail.com>
2025-06-02 16:57:44 -0300

    feat: add new commands

--- user_data.sh
+++ user_data.sh
@@ -2,6 +2,10 @@
 
 yum update -y
 
+sudo yum install -y docker
+
+sudo service docker start
+
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
@@ -11,3 +15,9 @@ cd /
 sudo mkdir projeto-compass
  
 cd projeto-compass
+
+sudo git clone https://github.com/Thullyoo/Compass-UOL-Projeto-2.git
+
+cd Compass-UOL-Projeto-2
+
+sudo docker compose up
