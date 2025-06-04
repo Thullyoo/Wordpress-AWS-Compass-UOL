18434758513402b41db74fc9775494622ef892d8
Thullyoo <thullyofilho@gmail.com>
2025-06-04 16:53:04 -0300

    feat: adjust user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-echo "DB_URL=" > .env
-
 sudo yum update -y
-
 sudo yum install -y docker
-
-sudo service docker start
-
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-cd /
-
-sudo mkdir projeto-compass
- 
-cd projeto-compass
-
 sudo yum install -y git
 
-sudo git clone https://github.com/Thullyoo/Compass-UOL-Projeto-2.git
+sudo yum install -y nfs-utils
 
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+cd / 
+
+sudo mkdir -p /mnt/efs
+
+sudo mount -t nfs4 -o nfsvers=4.1 DNSAQUI:/ /mnt/efs
+
+sudo docker volume create \
+  --driver local \
+  --opt type=none \
+  --opt device=/mnt/efs/wordpress \
+  --opt o=bind \
+  wp-efs
+
+sudo mkdir projeto-compass
+cd projeto-compass
+
+sudo git clone https://github.com/Thullyoo/Compass-UOL-Projeto-2.git
 cd Compass-UOL-Projeto-2
 
-sudo docker-compose up
+echo "DB_URL=seuendpointaqui" | sudo tee .env > /dev/null
+
+sudo docker-compose up -d
+
